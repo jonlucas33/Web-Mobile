b4b6c81dc833358d5344e37112c3fccf71ace2ad
João Lucas Lopes <joaolucaslopes296@gmail.com>
2023-05-03 13:00:52 -0300

    atvFetch finished.

--- atvFetch/index.js
+++ atvFetch/index.js
@@ -1,6 +1,24 @@
 const cep = document.querySelector("#cep")
 
+const showData = (result) => {
+  for (const campos in result) {
+    const idCampo = document.querySelector("#" + campos)
+    if (idCampo) {
+      idCampo.value = result[campos]
+    }
+  }
+}
+
 cep.addEventListener("blur", (e) => {
   let search = cep.value.replace("-", "")
-  console.log(cep.value)
+  const options = {
+    method: "GET",
+    mode: "cors",
+    cache: "default",
+  }
+  fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then((response) => {
+      response.json().then((data) => showData(data))
+    })
+    .catch((e) => console.log("Deu erro: " + e, message))
 })
