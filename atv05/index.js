e2310abdf8f19da2c631526969b164de50e4378f
João Lucas Lopes <joaolucaslopes296@gmail.com>
2023-05-16 00:17:39 -0300

    Ícone hamburguer feito e adição de acessibilidades

--- atv05/index.js
+++ atv05/index.js
@@ -37,8 +37,20 @@ button.addEventListener("click", () => {
 
 const btnMobile = document.querySelector("#btnMobile")
 
-function toggleMenu(params) {
+function toggleMenu(event) {
+  if (event.type === "touchstart") {
+    event.preventDefault()
+  }
   const nav = document.querySelector("#nav")
   nav.classList.toggle("active")
+  const active = nav.classList.contains("active")
+  event.currentTarget.setAttribute("aria-expanded", active)
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu")
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir menu")
+  }
 }
+
 btnMobile.addEventListener("click", toggleMenu)
+btnMobile.addEventListener("touchstart", toggleMenu)
